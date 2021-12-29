0ae7eb4f75f6ddc85b4db423ff2e915d47d78c71
0xinsanity <noahsahar@gmail.com>
2021-12-29 00:44:31 -0500

    updated address

--- src/components/useWeb3Contract.tsx
+++ src/components/useWeb3Contract.tsx
@@ -2,7 +2,7 @@ import { useCallback } from 'react'
 import Web3 from 'web3'
 
 export const ABC_TREASURY_ADDRESS = "0x93dcf21284B78163ec218DeF37471C4B77CCC6E1"
-export const ABC_PRICING_SESSION_ADDRESS = "0x92cFCbc96543134823DcB83b71a94465108E56E0"
+export const ABC_PRICING_SESSION_ADDRESS = "0xfc3232FF3e85112F231ee72c28b7f197088Efa24"
 export const SUBGRAPH = process.env.GATSBY_SUBGRAPH as string
 const ARBITRUM_ETH_RPC = "https://arbitrum-mainnet.infura.io/v3/b2d15a1424b74f158a3ccf9f78f2e8e0"
 
